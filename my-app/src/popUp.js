04cbeccbf3e495d467db9363ae951881228c7a5b
YeibinKang <yeibinkang@gmail.com>
2021-07-06 22:23:33 -0400

    added Modal into Reservation component

---
+++ my-app/src/popUp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function popUp(props){
+    return(
+        <div className="popup">
+            <div className="popup-inner">
+            
+            </div>
+
+        </div>
+    )
+}
+
+export default popUp;
