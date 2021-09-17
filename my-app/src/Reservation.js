6d11ee8df67b3c67fa8171e6408729378042f2a9
YeibinKang <yeibinkang@gmail.com>
2021-09-16 23:17:32 -0400

    editing the server side code

--- my-app/src/Reservation.js
+++ my-app/src/Reservation.js
@@ -76,9 +76,9 @@ function Reservation(){
       <Button variant="primary" onClick={handleShow}>Create</Button>
       <Modal show={show} onHide={handleClose} animation={false}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Reservation confirmation</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body>Thanks for reservation</Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
